e75a710ec821d4aa18a0dfa308c863781d181b4b
kingchan223 <kokiyo97@naver.com>
2021-11-17 12:54:44 +0900

    first

---
+++ src/main/java/com/backend/vueshop_server/VueshopServerApplication.java
@@ -0,0 +1,13 @@
+package com.backend.vueshop_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueshopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueshopServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/vueshop_server/VueshopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.vueshop_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueshopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
